cdf5ca6ee94bb3a2d5edb87f8bee8c711c0e14e3
Bigen-dev <m.hasbialghifariabi@gmail.com>
2026-09-10 21:30:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ form.addEventListener('submit', async function(e) {
   };
 
   try {
-    const response = await fetch('https://sheetdb.io/api/v1/kcp9x1kr758hg', {
+    const response = await fetch(URL_SHEETDB_ANDA, {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
